0522956e47a57b1334989dc6708ffd50347953e4
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2022-07-09 16:11:48 +0200

    Added Footer component.

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+    return (
+        <p
+            className="footer"
+            style={{cursor: "default"}}
+        >
+            &copy;Copyright plx 2022 | by CaolDev
+        </p>
+    )
+};
