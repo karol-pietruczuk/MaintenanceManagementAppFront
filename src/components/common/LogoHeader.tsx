1a0a97aa8e38f701bf7af18659b777ce91885986
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2022-07-09 16:14:19 +0200

    Added LogoHeader component.

--- src/components/common/LogoHeader.tsx
+++ src/components/common/LogoHeader.tsx
@@ -3,21 +3,35 @@ import './LogoHeader.css';
 
 interface Props {
     imageSize?: number;
+    menuMarginTop?: number;
+    logoFontSize?: number;
+    descriptionVisibility?: boolean;
 }
 
 export const LogoHeader = (props: Props) => {
     return (
-        <header>
+        <header
+            style = {{marginTop: props.menuMarginTop !== undefined ? props.menuMarginTop : 60}}
+        >
             <img
                 src={logo}
                 alt="plx logo"
-                width={props.imageSize ? props.imageSize : 100}
-                height={props.imageSize ? props.imageSize : 100}
+                width={props.imageSize !== undefined ? props.imageSize : 100}
+                height={props.imageSize !== undefined ? props.imageSize : 100
+
+                }
             />
-            <h1>
+            <h1
+            style={{fontSize: props.logoFontSize !== undefined ? props.logoFontSize : 24}}
+            >
                 <strong>plx</strong>
             </h1>
-            <h2>
+            <h2
+                style={{
+                    fontSize: 14,
+                    display: !props.descriptionVisibility ? "none" : "block"
+                }}
+            >
                 easier work, better work
             </h2>
         </header>
