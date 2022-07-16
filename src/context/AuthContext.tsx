1e76896b2aac7afb883caeb476b66ebac66fee33
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2022-07-16 11:25:26 +0200

    Added AuthContext.

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,12 +1,23 @@
-import { createContext } from "react";
+import {Context, createContext, Dispatch} from "react";
+import {SetUser, User} from "../types/user";
 
+// const user: User = {
+//     userEmail: '',
+//     userType: '',
+//     accessToken: '',
+//     refreshToken: '',
+// }
+//
+// const setUser: SetUser = (user: User): void => {}
+
+// export const AuthContext: Context<{ user: User, setUser: Dispatch<User> }> = createContext({user, setUser});
 export const AuthContext = createContext({
-    accessToken: '',
-    setAccessToken: (accessToken: string): void => {},
-    refreshToken: '',
-    setRefreshToken: (accessToken: string): void => {},
     userEmail: '',
-    setUserEmail: (email: string): void => {},
     userType: '',
-    setUserType: (userType: string): void => {},
+    accessToken: '',
+    refreshToken: '',
+    setUserEmail: (newUserEmail: string): void => {},
+    setUserType: (newUserType: string): void => {},
+    setAccessToken: (newAccessToken: string): void => {},
+    setRefreshToken: (newRefreshToken: string): void => {},
 });
