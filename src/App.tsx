f79e0d1493c632a0ef4aa091b407632de7e56ccc
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2022-07-16 11:29:22 +0200

    Added Routing and changed context.

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,42 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {Login} from "./components/layout/Login/Login";
 import {AuthContext} from "./context/AuthContext";
+import {Tasks} from "./components/layout/Tasks/Tasks";
+import {Route, Routes, Navigate} from "react-router-dom";
+import {Admin} from "./components/layout/Admin/Admin";
+import {Dashboard} from "./components/layout/Dashboard/Dashboard";
+import {Parts} from "./components/layout/Parts/Parts";
+import {Orders} from "./components/layout/Orders/Orders";
+import {Blank} from "./components/layout/Blank/Blank";
+import {User} from "./types/user";
 
 export const App = () => {
-  const [accessToken, setAccessToken] = useState('');
-  const [refreshToken, setRefreshToken] = useState('');
-  const [userEmail, setUserEmail] = useState('');
-  const [userType, setUserType] = useState('');
+  // const [user, setUser] = useState({
+  //     userEmail: '',
+  //     userType: '',
+  //     accessToken: '',
+  //     refreshToken: '',
+  // });
+    const [userEmail, setUserEmail] = useState('');
+    const [userType, setUserType] = useState('');
+    const [accessToken, setAccessToken] = useState('');
+    const [refreshToken, setRefreshToken] = useState('');
 
   return (
       <AuthContext.Provider
-          value={{
-              accessToken,
-              setAccessToken,
-              refreshToken,
-              setRefreshToken,
-              userEmail,
-              setUserEmail,
-              userType,
-              setUserType}}
+          value={{userEmail, userType, accessToken, refreshToken, setUserEmail, setUserType, setAccessToken, setRefreshToken}}
       >
-        <Login/>
+          <Routes>
+              <Route path="/login" element={<Login/>}/>
+              <Route path="/admin" element={<Admin/>}/>
+              <Route path="/dashboard" element={<Dashboard/>}/>
+              <Route path="/task" element={<Tasks/>}/>
+              <Route path="/part" element={<Parts/>}/>
+              <Route path="/order" element={<Orders/>}/>
+              <Route path="/" element={userEmail && userType && accessToken && refreshToken ? <Navigate to="/task" /> : <Navigate to="/login" />}/>
+              <Route path={document.URL.replace('http://localhost:3000', '')} element={<Blank/>}/>
+          </Routes>
       </AuthContext.Provider>
   )
 };
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
